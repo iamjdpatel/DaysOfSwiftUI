ddb956d01e41bb8024988e22d7d028970fafd7b3
JD Patel <iamjdpatel@gmail.com>
2020-06-30 23:12:39 +0530

    Project 4 - BetterRest

---
+++ 04-BetterRest/BetterRest/Shared/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  Shared
+//
+//  Created by JD on 29/06/20.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
