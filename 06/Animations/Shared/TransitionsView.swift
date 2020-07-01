5656cf4de4e0f9634848f80228881d4482636f7c
JD Patel <iamjdpatel@gmail.com>
2020-07-01 23:56:47 +0530

    Project 6 - Animations

--- 06/Animations/Shared/TransitionsView.swift
+++ 06/Animations/Shared/TransitionsView.swift
@@ -24,11 +24,31 @@ struct TransitionsView: View {
                 Rectangle()
                     .fill(Color.red)
                     .frame(width: 200, height: 200)
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+//                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)         //Custom Transition
+
             }
             
         }
         
     }
     
+}
+
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content.rotationEffect(.degrees(amount), anchor: anchor).clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
 }
