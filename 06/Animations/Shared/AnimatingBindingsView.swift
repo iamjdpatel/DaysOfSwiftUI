d60acdc47f0c06e5cd53cb8145c39b98da7e85bd
JD Patel <iamjdpatel@gmail.com>
2020-07-01 22:54:35 +0530

    Project 6 - Animations

---
+++ 06/Animations/Shared/AnimatingBindingsView.swift
@@ -0,0 +1,28 @@
+//
+//  AnimatingBindingsView.swift
+//  Animations
+//
+//  Created by JD on 01/07/20.
+//
+
+import SwiftUI
+
+struct AnimatingBindingsView: View {
+    
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            VStack(spacing: 150) {
+                
+                
+            }.padding(.all, 10)
+            
+            .navigationBarTitle(Text("Animating Bindings"), displayMode: .inline)
+
+        }
+        
+    }
+    
+}
--- 06/Animations/Shared/ContentView.swift
+++ 06/Animations/Shared/ContentView.swift
@@ -8,9 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var defaultAnimationAmount: CGFloat = 1
+    @State private var easeInOutAnimationAmount: CGFloat = 1
+    @State private var animationAmount: CGFloat = 1
+
     var body: some View {
-        Text("Hello, world!").padding()
+        
+        NavigationView {
+            
+            VStack(spacing: 30) {
+                
+                Button("Default") {
+                    self.defaultAnimationAmount += 1
+                }
+                .padding(50)
+                .background(Color.red)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .scaleEffect(defaultAnimationAmount)
+                .animation(.default)
+                .blur(radius: (defaultAnimationAmount - 1) * 3)
+                
+                Button("Custom") {
+                    self.easeInOutAnimationAmount += 1
+                }
+                .padding(40)
+                .background(Color.red)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .overlay(
+                    Circle()
+                        .stroke(Color.red)
+                        .scaleEffect(easeInOutAnimationAmount)
+                        .opacity(Double(2 - easeInOutAnimationAmount))
+                        .animation(
+                            Animation.easeOut(duration: 1)
+                                .repeatForever(autoreverses: false)
+                        )
+                )
+                
+                Divider()
+                
+                Stepper("Scale amount", value: $animationAmount.animation(
+                    Animation.easeInOut(duration: 1)
+                        .repeatCount(3, autoreverses: true)
+                ), in: 1...10)
+                
+                Button("Tap Me") {
+                    self.animationAmount += 1
+                }
+                .padding(40)
+                .background(Color.red)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .scaleEffect(animationAmount)
+                
+                
+//                Spacer()
+                
+            }
+            .navigationBarTitle(Text("Animations"), displayMode: .inline)
+            
+        }
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
