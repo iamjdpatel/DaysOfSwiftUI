70b1e4e0a890641363dc95596675c91e06a4afc1
JD Patel <iamjdpatel@gmail.com>
2020-07-04 02:02:46 +0530

    Project 13 - Instafilter

--- 13/Instafilter/Shared/HomeView.swift
+++ 13/Instafilter/Shared/HomeView.swift
@@ -13,7 +13,7 @@ struct HomeView: View {
     
     @State private var image: Image?
     @State private var showingImagePicker = false
-
+    
     @State private var inputImage: UIImage?
     
     @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
@@ -26,7 +26,7 @@ struct HomeView: View {
     }
     
     @State private var showingFilterSheet = false
-
+    
     @State private var processedImage: UIImage?
     
     var body: some View {
@@ -41,43 +41,55 @@ struct HomeView: View {
             }
         )
         
-        return VStack(spacing: 30) {
-            image?
-                .resizable()
-                .scaledToFit()
-
-            Button("Select Image") {
-               self.showingImagePicker = true
-            }
+        return VStack {
             
-            Button("Change Filter") {
-               self.showingFilterSheet = true
-            }
+            Spacer()
             
-            Button("Save") {
-                guard let processedImage = self.processedImage else { return }
+            VStack(spacing: 30) {
+                image?
+                    .resizable()
+                    .scaledToFit()
                 
-                let imageSaver = ImageSaver()
-
-                imageSaver.successHandler = {
-                    print("Success!")
-                }
-
-                imageSaver.errorHandler = {
-                    print("Oops: \($0.localizedDescription)")
+                Button("Select Image") {
+                    self.showingImagePicker = true
                 }
                 
-                imageSaver.writeToPhotoAlbum(image: processedImage)
+                Button("Change Filter") {
+                    self.showingFilterSheet = true
+                }
+                
+                Button("Save") {
+                    guard let processedImage = self.processedImage else { return }
+                    
+                    let imageSaver = ImageSaver()
+                    
+                    imageSaver.successHandler = {
+                        print("Success!")
+                    }
+                    
+                    imageSaver.errorHandler = {
+                        print("Oops: \($0.localizedDescription)")
+                    }
+                    
+                    imageSaver.writeToPhotoAlbum(image: processedImage)
+                }
+                
+                Slider(value: intensity)
+                    .padding()
             }
             
-            Slider(value: intensity)
-                .padding()
+            Spacer()
+            
+            Text("JD")
+                .font(.caption)
+                .fontWeight(.light)
+                .foregroundColor(.gray)
             
         }
         .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
             ImagePicker(image: self.$inputImage)
         }
-            
+        
         .actionSheet(isPresented: $showingFilterSheet) {
             ActionSheet(title: Text("Select a filter"), buttons: [
                 .default(Text("Crystallize")) { self.setFilter(CIFilter.crystallize()) },
@@ -95,7 +107,7 @@ struct HomeView: View {
     
     func loadImage() {
         guard let inputImage = inputImage else { return }
-
+        
         let beginImage = CIImage(image: inputImage)
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
         applyProcessing()
@@ -109,7 +121,7 @@ struct HomeView: View {
         if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
         
         guard let outputImage = currentFilter.outputImage else { return }
-
+        
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
             let uiImage = UIImage(cgImage: cgimg)
             image = Image(uiImage: uiImage)
@@ -126,4 +138,9 @@ struct HomeView: View {
 }
 
 
-
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+            .preferredColorScheme(.dark)
+    }
+}
