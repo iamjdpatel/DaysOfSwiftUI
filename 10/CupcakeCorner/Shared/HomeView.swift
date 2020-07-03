556ca96adf83a47b39b457bee363ef723c2ad506
JD Patel <iamjdpatel@gmail.com>
2020-07-04 01:53:21 +0530

    Project 10 - CupcakeCorner

--- 10/CupcakeCorner/Shared/HomeView.swift
+++ 10/CupcakeCorner/Shared/HomeView.swift
@@ -10,46 +10,55 @@ import SwiftUI
 struct HomeView: View {
     
     @ObservedObject var order = Order()
-
+    
     var body: some View {
         
         NavigationView {
             
-            Form {
+            VStack {
                 
-                Section {
-                    Picker("Select your cake type", selection: $order.type) {
-                        ForEach(0..<Order.types.count, id: \.self) {
-                            Text(Order.types[$0])
+                Form {
+                    
+                    Section {
+                        Picker("Select your cake type", selection: $order.type) {
+                            ForEach(0..<Order.types.count, id: \.self) {
+                                Text(Order.types[$0])
+                            }
+                        }
+                        
+                        Stepper(value: $order.quantity, in: 3...20) {
+                            Text("Number of cakes: \(order.quantity)")
                         }
                     }
-
-                    Stepper(value: $order.quantity, in: 3...20) {
-                        Text("Number of cakes: \(order.quantity)")
-                    }
-                }
-                
-                Section {
-                    Toggle(isOn: $order.specialRequestEnabled.animation()) {
-                        Text("Any special requests?")
-                    }
-
-                    if order.specialRequestEnabled {
-                        Toggle(isOn: $order.extraFrosting) {
-                            Text("Add extra frosting")
+                    
+                    Section {
+                        Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                            Text("Any special requests?")
                         }
-
-                        Toggle(isOn: $order.addSprinkles) {
-                            Text("Add extra sprinkles")
+                        
+                        if order.specialRequestEnabled {
+                            Toggle(isOn: $order.extraFrosting) {
+                                Text("Add extra frosting")
+                            }
+                            
+                            Toggle(isOn: $order.addSprinkles) {
+                                Text("Add extra sprinkles")
+                            }
                         }
                     }
+                    
+                    Section {
+                        NavigationLink(destination: AddressView(order: order)) {
+                            Text("Delivery details")
+                        }
+                    }
+                    
                 }
                 
-                Section {
-                    NavigationLink(destination: AddressView(order: order)) {
-                        Text("Delivery details")
-                    }
-                }
+                Text("JD")
+                    .font(.caption)
+                    .fontWeight(.light)
+                    .foregroundColor(.gray)
                 
             }
             .navigationBarTitle("Cupcake Corner", displayMode: .large)
